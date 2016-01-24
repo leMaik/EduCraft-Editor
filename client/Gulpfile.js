915a25def49ecc9ce7cf2da54703b0f5e6119645
leMaik <an@maik.tk>
2016-01-24 13:42:58 +0100

    Cleanup. Add config for systemjs builder.

--- client/Gulpfile.js
+++ client/Gulpfile.js
@@ -8,27 +8,47 @@ var gutil = require('gulp-util');
 var uglify = require('gulp-uglify');
 var sourcemaps = require('gulp-sourcemaps');
 var copy = require('gulp-copy');
+var Builder = require('systemjs-builder');
 
-gulp.task('javascript', function () {
-  // set up the browserify instance on a task basis
-  var b = browserify({
-    debug: true,
-    entries: ['./js/client.js']
-  });
-
-  return b.bundle()
-    .pipe(source('./js/client.js'))
-    .pipe(buffer())
-    .pipe(sourcemaps.init({loadMaps: true}))
-        .pipe(uglify())
-        .on('error', gutil.log)
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./dist/'));
+gulp.task('javascript', function (done) {
+    var builder = new Builder();
+    builder
+        .config({
+            baseUrl: './',
+            transpiler: 'typescript',
+            typescriptOptions: {
+                'module': 'commonjs',
+                resolveTypings: true,
+                emitDecoratorMetadata: true,
+                sourceMap: true,
+                inlineSourceMap: false
+            },
+            packages: {
+                ts: {
+                    main: 'app',
+                    defaultExtension: 'ts'
+                }
+            },
+            paths: {
+                'angular2/*': 'node_modules/angular2/*.js',
+                'rxjs/operator/*': 'node_modules/rxjs/add/operator/*.js',
+                'rxjs/*': 'node_modules/rxjs/*.js',
+                'app': 'ts'
+            },
+            map: {
+                'typescript': 'node_modules/typescript/lib/typescript.js'
+            },
+            defaultJSExtensions: true
+        });
+    builder.buildStatic('app/boot', 'dist/app.js', {minify: true, sourceMaps: true})
+        .then(function () {
+            done();
+        });
 });
 
-gulp.task('copy', function() {
+gulp.task('copy', function () {
     return gulp.src(['./index.html', './css/*'])
-      .pipe(copy('./dist/'));
+        .pipe(copy('./dist/'));
 });
 
 gulp.task('default', ['javascript', 'copy']);
--- client/js/client.js
+++
@@ -1,7 +0,0 @@
-var ace = require('brace');
-require('brace/mode/lua');
-require('brace/theme/tomorrow_night');
-
-var editor = ace.edit("editor");
-editor.getSession().setMode("ace/mode/lua");
-editor.setTheme("ace/theme/tomorrow_night");
