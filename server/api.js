898347b89f4228c3729ae682ec6ab6739ea6fea9
Maik Marschner <an@maik.tk>
2016-03-16 17:33:06 +0100

    Small avatar fix.

--- server/api.js
+++ server/api.js
@@ -17,8 +17,9 @@ module.exports = (app, models) => {
         const id = req.user.oauthId;
         if (id.indexOf('cf-') === 0) {
             res.redirect('https://forum.craften.de/data/avatars/m/0/' + id.substr(3) + '.jpg').end();
+        } else {
+            res.status(404).end();
         }
-        res.status(404).end();
     });
 
     app.get('/api/modules', (req, res)=> {
